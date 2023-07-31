82b5dde9982eb67e9c77059846914816245f1b79
crossbanc <140584833+crossbanc@users.noreply.github.com>
2023-07-31 20:47:24 +0800

    Add files via upload

--- js/hasher.js
+++ js/hasher.js
@@ -1,47 +1,67 @@
+document.addEventListener("click", (e) => {
+    const { target } = e;
+    if (!target.matches("hashnav a")) {
+        return;
+    }
+    e.preventDefault();
+    route2();
+});
+
+
 const hashes = {
-    feedback:
+    "/feedback":
     {
-        template: "/pages/hashes/feedback.html",
+        template: "/pages/#/feedback.html",
         title: "Whitepaper(s) on crossbanc | Questions",
         description: "Reach out to crossbanc with your questions.",
     },
-    interpretation: {
-        template: "/pages/hashes/interpretation.html",
+    "/interpretation": {
+        template: "/pages/#/interpretation.html",
         title: "Whitepaper(s) on crossbanc | Interpretation",
         description: "Defined terms used to communicate crossbanc.",
     },
-    statements: {
-        template: "/pages/hashes/statements.html",
+    "/statements": {
+        template: "/pages/#/statements.html",
         title: "Whitepaper(s) on crossbanc | Questions Presented",
         description: "Problem Statement(s) perceived and expected to be solved by crossbanc.",
     },
-    basis: {
-        template: "/pages/hashes/basis.html",
+    "/basis": {
+        template: "/pages/#/basis.html",
         title: "Whitepaper(s) on crossbanc | Basis for conclusions",
         description: "Basis for conclusions on technology adoptions to the benefit of crossbanc stakeholders.",
     },
-    disclaimer: {
-        template: "/pages/hashes/disclaimer.html",
+    "/disclaimer": {
+        template: "/pages/#/disclaimer.html",
         title: "Whitepaper(s) on crossbanc | Disclaimer",
         description: "Disclaimer in whitepaper(s) regarding crossbanc.",
     },
 };
 
-const hashlocationHandler = async () => {
-    var hashlocation = window.location.hash.replace("#", "");   
-    if (hashlocation == undefined || hashlocation == null || hashlocation.length == 0) {
-        hashlocation = "feedback"
-    }
-    const hash = hashes[hashlocation];
-    const hhtml = await fetch(hash.template).then((response) => response.text());
-        
-    document.getElementById("hcontent").innerHTML = hhtml;
-    document.title = hash.title;
-    document
-        .querySelector('meta[name="description"]')
-        .setAttribute("content", hash.description);
+
+const route2 = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    hashlocationHandler();
 };
 
-window.addEventListener("hashchange", hashlocationHandler);
+
+const hashlocationHandler = async () => {
+    const hashlocation = window.location.pathname; 
+    if (hashlocation.length == 0) {
+        hashlocation = "/feedback";
+    }
+    const hroute = hashes[location];
+    const hhtml = await fetch(hroute.template).then((response) => response.text());
+    document.getElementById("hcontent").innerHTML = hhtml;
+    document.title = hroute.title;
+    document
+        .querySelector('meta[name="description"]')
+        .setAttribute("content", hroute.description);
+};
+
+
+window.onpopstate = hashlocationHandler;
+window.route = route2;
 hashlocationHandler();
 
