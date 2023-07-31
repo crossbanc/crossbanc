7f1406ce1326c233b60a3d031281cbbbc832a0d8
crossbanc <140584833+crossbanc@users.noreply.github.com>
2023-07-31 21:04:41 +0800

    Add files via upload

--- js/hasher.js
+++ js/hasher.js
@@ -9,29 +9,28 @@ document.addEventListener("click", (e) => {
 
 
 const hashes = {
-    "/feedback":
-    {
-        template: "/pages/#/feedback.html",
+    "/feedback": {
+        template: "/pages/feedback.html",
         title: "Whitepaper(s) on crossbanc | Questions",
         description: "Reach out to crossbanc with your questions.",
     },
     "/interpretation": {
-        template: "/pages/#/interpretation.html",
+        template: "/pages/interpretation.html",
         title: "Whitepaper(s) on crossbanc | Interpretation",
         description: "Defined terms used to communicate crossbanc.",
     },
     "/statements": {
-        template: "/pages/#/statements.html",
+        template: "/pages/statements.html",
         title: "Whitepaper(s) on crossbanc | Questions Presented",
         description: "Problem Statement(s) perceived and expected to be solved by crossbanc.",
     },
     "/basis": {
-        template: "/pages/#/basis.html",
+        template: "/pages/basis.html",
         title: "Whitepaper(s) on crossbanc | Basis for conclusions",
         description: "Basis for conclusions on technology adoptions to the benefit of crossbanc stakeholders.",
     },
     "/disclaimer": {
-        template: "/pages/#/disclaimer.html",
+        template: "/pages/disclaimer.html",
         title: "Whitepaper(s) on crossbanc | Disclaimer",
         description: "Disclaimer in whitepaper(s) regarding crossbanc.",
     },
